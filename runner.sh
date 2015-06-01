650f7a3e0291b0708e42d36da641701f1a3988b9
f41lurizer <forumf41l@gmail.com>
2015-05-31 23:43:29 -0500

    runner started

---
+++ runner.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Author: Mazin Jindeel
+#Date: 6/15
+#Functionality: This is a script to find the most interconnected wikipedia page. It will run all of the provided tools with default settings. It takes two arguments -- the first argument is the path to the uncompressed data from wikipedia (download links in the README document). The second is the path of the output file. For example, it may be run like this
+bash runner.sh wikipedia.xml results.txt
+
